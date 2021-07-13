4e28ab4a67abba65f0cc6922a764815b698cca7b
unknown <lucassporte_12@hotmail.com>
2021-07-12 22:29:01 -0300

    aula1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+  
+  @media(min-width: 860px){
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 618px 312px;
+
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea: "profileArea"}}>
+      <Box >
+        Imagem
+      </Box>
+      </div>
+      <div style={{ gridArea: "welcomeArea"}}>
+      <Box > 
+        Bem vindo
+      </Box>
+      </div>
+      <div style={{ gridArea: "profileRelationsArea"}}>
+      <Box >
+        Comunidades
+      </Box>
+      </div>
+    </MainGrid>
+  )
 }
